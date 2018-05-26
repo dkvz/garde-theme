78ff15f4064e84f52b247098aad1ca99264f4a79
DkVZ <git@dkvz.eu>
2018-05-26 16:58:36 +0200

    CSS extraction is now working, thank god

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 // https://github.com/jalkoby/sass-webpack-plugin
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const contentBase = path.join(__dirname, 'dist');
 
 module.exports = {
@@ -11,20 +11,35 @@ module.exports = {
     path: contentBase,
     filename: '[name].js'
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
   module: {
     rules: [{
       test: /\.scss$/,
-      loader: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: ['css-loader','postcss-loader','sass-loader']
-      })
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
     }]
   },
   plugins: [
     new HtmlPlugin({
       template: './src/index.html',
     }),
-    new ExtractTextPlugin("[name][contenthash].css")
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    })
   ],
   devServer: (process.env.NODE_ENV === 'production') ? false : {
     contentBase: contentBase,
